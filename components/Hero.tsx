cf20674a3a22718090f3df8cf4ac64a092373550
Maxime Guillaumin <57984946+emgee97@users.noreply.github.com>
2026-02-19 09:48:02 +0100

    Update Hero.tsx
    Update my city

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Maxime, a Full-Stack Developer based in France.
+            Hi! I&apos;m Maxime, a Full-Stack Developer based in Grenoble, France.
           </p>
 
           <a href="#about">
