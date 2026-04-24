899ad7ac79fd53432cdc6baf5326597144d0a2df
emgee97 <mguillaumin97@gmail.com>
2026-04-24 14:10:34 +0200

    Fix image position: show top on mobile for maxime.png

--- data/index.ts
+++ data/index.ts
@@ -5,7 +5,7 @@ export const gridItemsStructure = [
   {
     id: 1,
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-    imgClassName: "w-full h-full",
+    imgClassName: "w-full h-full object-top md:object-center",
     titleClassName: "justify-end",
     img: "/maxime.png",
     spareImg: "",
